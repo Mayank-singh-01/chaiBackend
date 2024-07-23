292f47314824526e7de2564bde5289e9ebb47fe9
Mayank singh <mayank23378@gmail.com>
2024-07-23 23:07:00 +0530

    logic building

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,20 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req,res)=>{
-   res.status(200).json({
-      massage: "welcome to backend"
-   })
+   // get user details from frontend 
+   // validation - not empty 
+   // checkif user already exists : username nad email
+   //check for image nad avatar 
+   //uplosad them on cloudnary : avatar 
+   //creat user object and entry in data
+   //remove password and refresh token field from response 
+   // check for user creation
+   //return response
+
+   const { fullName,email,username,password } = req.body
+   console.log(`email: ${email}`);
+
+
 } )
 
 export {registerUser}
