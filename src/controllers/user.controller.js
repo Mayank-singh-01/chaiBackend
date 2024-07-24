a40b9b072507833e5114fc8650e981fc54e1329b
Mayank singh <mayank23378@gmail.com>
2024-07-24 21:13:18 +0530

    done with login logic building

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,24 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
+const generateAccessTokenAndRefreshToken = async(userId) => {
+   try {
+      const user =  await User.findById(userId)
+      const accessToken = user.generateAccessToken()
+      const refreshToken = user.generateRefreshToken()
+
+      user.refreshToken = refreshToken;
+      await user.save({ validateBeforeSave: false });
+
+      return { accessToken, refreshToken };
+
+   } catch (error) {
+      throw new ApiError(500,"Error while generating refresh and access token")
+   }
+}
+
+
+
 const registerUser = asyncHandler( async (req,res)=>{
    // get user details from frontend 
    // validation - not empty 
@@ -17,7 +35,7 @@ const registerUser = asyncHandler( async (req,res)=>{
    //return response
 
    const { fullName,email,username,password } = req.body
-   console.log(`email: ${email}`);
+   // console.log(`email: ${email}`);
 
 
    if (
@@ -38,7 +56,12 @@ const registerUser = asyncHandler( async (req,res)=>{
 
 
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   let coverImageLocalPath;
+   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path;
+   }
 
    if (!avatarLocalPath) {
       throw new ApiError(400,"Avatar is required")
@@ -75,6 +98,65 @@ const registerUser = asyncHandler( async (req,res)=>{
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
+} );
+
+const loginUser = asyncHandler( async (req,res) => {
+   //req body -> data 
+   //username or email
+   //find the user 
+   //password check
+   //access and refresh token
+   //send cookie
+
+
+   const { email,username,password } = req.body
+
+   if (!username || !email) {
+      throw new ApiError(400,"Username or email is required");
+   }
+
+   const user = await User.findOne({
+      $or: [{username},{email}]
+   })
+
+   if (!user) {
+      throw new ApiError(404,"user no exist")
+   }
+
+   const isPasswordValid =  await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+      throw new ApiError(404, "invalid user password");
+    }
+
+    const {accessToken, refreshToken} = await generateAccessTokenAndRefreshToken(user._id)
+
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+     const options = {
+       httpOnly: true,
+       secure: true,
+     };
+
+      return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+
+
 } )
 
-export {registerUser}
+export { registerUser, loginUser };
+
+
